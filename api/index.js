f21a44e7a9a075b4a1fe8a3fc54916fe0398e3d8
NGASSAI <comptengassai@gmail.com>
2026-06-16 12:05:26 +0100

    Fix: Utilisation de export default pour ES Modules

--- api/index.js
+++ api/index.js
@@ -6,11 +6,3 @@ const { default: app } = await import('../backend/src/app.js');
 
 // Export ESM (pour les environnements qui l'utilisent)
 export default app;
-
-// Export CommonJS strict requis par certains adaptateurs :
-// on l'ajoute ici de façon sûre (ne lèvera pas en ESM pur si `module` est undefined).
-try {
-	module.exports = app;
-} catch (e) {
-	// ignore
-}
